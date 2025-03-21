c977c907cb1c711948b484b9554f49ace6671cac
aspirin <f1193968991@126.com>
2025-03-21 18:34:04 +0800

    feat: get single post

--- crates/aftershock_storage/src/error.rs
+++ crates/aftershock_storage/src/error.rs
@@ -7,12 +7,18 @@ pub enum Error {
     DatabasePoolError(#[from] r2d2::Error),
     #[error("Database Error: {0}")]
     DatabaseError(#[from] diesel::result::Error),
+
+    #[error("Not Found: {0}")]
+    NotFound(String),
 }
 
 impl IntoResponse for Error {
     fn into_response(self) -> axum::response::Response {
         let msg = format!("{self}");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, msg).into_response()
+        match self {
+            Self::NotFound(msg) => (StatusCode::NOT_FOUND, msg).into_response(),
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, msg).into_response(),
+        }
     }
 }
--- crates/aftershock_storage/src/lib.rs
+++ crates/aftershock_storage/src/lib.rs
@@ -1,24 +1,22 @@
-use axum::Json;
+use axum::{Json, extract::Path};
 use diesel::prelude::*;
 use models::{Content, NewContent};
 use pool::{DbPool, get_connection_pool};
 use std::sync::LazyLock;
 
+pub mod error;
 pub mod models;
 pub mod pool;
 pub mod schema;
-pub mod error;
 
 type Result<T> = std::result::Result<T, error::Error>;
 
-static POOL: LazyLock<DbPool> = LazyLock::new(|| get_connection_pool());
+static POOL: LazyLock<DbPool> = LazyLock::new(get_connection_pool);
 
 pub async fn get_posts() -> Result<Json<Vec<aftershock_bridge::Post>>> {
     use schema::contents::dsl::*;
 
-    let conn = &mut POOL
-        .clone()
-        .get()?;
+    let conn = &mut POOL.clone().get()?;
     let results = contents
         .filter(published.eq(true))
         .select(Content::as_select())
@@ -28,12 +26,27 @@ pub async fn get_posts() -> Result<Json<Vec<aftershock_bridge::Post>>> {
     Ok(Json(ret))
 }
 
+pub async fn get_post(Path(post_id): Path<i32>) -> Result<Json<Content>> {
+    use schema::contents::dsl::*;
+
+    let conn = &mut POOL.clone().get()?;
+
+    let mut ret = contents
+        .filter(id.eq(post_id))
+        .limit(1)
+        .select(Content::as_select())
+        .load(conn)?;
+    if ret.is_empty() {
+        Err(error::Error::NotFound("Post not found".into()))
+    } else {
+        Ok(Json(ret.pop().unwrap()))
+    }
+}
+
 pub async fn create_post(Json(post): Json<aftershock_bridge::NewPost>) -> Result<Json<Content>> {
     use schema::contents::dsl::*;
 
-    let conn = &mut POOL
-        .clone()
-        .get()?;
+    let conn = &mut POOL.clone().get()?;
     let new_content: NewContent = (&post).into();
 
     let ret = diesel::insert_into(contents)
