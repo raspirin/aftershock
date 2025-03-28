1899158e993ce518a9e01daa59a57ce4785d0bfd
aspirin <f1193968991@126.com>
2025-03-29 00:17:10 +0800

    feat: home page with message box

--- crates/aftershock/src/app/pages/home_page.rs
+++ crates/aftershock/src/app/pages/home_page.rs
@@ -1,10 +1,13 @@
 use leptos::prelude::*;
 
-use crate::app::components::PostMetaList;
+use crate::app::components::{MessageBox, PostMetaList};
 use crate::app::server::get_published_posts_meta;
+use crate::MSG_LOAD_DATA_FAILURE;
 
 #[component]
 pub fn HomePage() -> impl IntoView {
+    let (msg, _) = signal(String::from(MSG_LOAD_DATA_FAILURE));
+
     view! {
         <div class:post_meta_container>
             <Await future=get_published_posts_meta() let:data>
@@ -13,7 +16,7 @@ pub fn HomePage() -> impl IntoView {
                         let posts = s.clone();
                         view! { <PostMetaList post_meta_list=posts /> }.into_any()
                     }
-                    Err(_) => view! { "Fail to load posts." }.into_any(),
+                    Err(_) => view! { <MessageBox msg=msg /> }.into_any(),
                 }}
             </Await>
         </div>
