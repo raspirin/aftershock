f44a52fa3a5b0777db1f8c64025d5e9498e49fdd
aspirin <f1193968991@126.com>
2025-03-30 01:15:15 +0800

    feat: showing a msg box when there is no post

--- crates/aftershock/src/app/pages/home_page.rs
+++ crates/aftershock/src/app/pages/home_page.rs
@@ -2,21 +2,31 @@ use leptos::prelude::*;
 
 use crate::app::components::{MessageBox, PostMetaList};
 use crate::app::server::get_published_posts_meta;
-use crate::MSG_LOAD_DATA_FAILURE;
+use crate::{MSG_ARCHIVE_PLACEHOLDER, MSG_LOAD_DATA_FAILURE};
 
 #[component]
 pub fn HomePage() -> impl IntoView {
-    let (msg, _) = signal(String::from(MSG_LOAD_DATA_FAILURE));
+    let (msg, set_msg) = signal(String::from(MSG_LOAD_DATA_FAILURE));
 
     view! {
         <div class:post_meta_container>
             <Await future=get_published_posts_meta() let:data>
                 {match data {
-                    Ok(s) => {
+                    Ok(s) if !s.is_empty() => {
                         let posts = s.clone();
                         view! { <PostMetaList post_meta_list=posts /> }.into_any()
                     }
-                    Err(_) => view! { <MessageBox msg=msg /> }.into_any(),
+                    Ok(_) => {
+                        *set_msg.write() = MSG_ARCHIVE_PLACEHOLDER.into();
+                        {
+                            view! { <MessageBox msg=msg /> }
+                        }
+                            .into_any()
+                    }
+                    Err(_) => {
+                        *set_msg.write() = MSG_LOAD_DATA_FAILURE.into();
+                        view! { <MessageBox msg=msg /> }.into_any()
+                    }
                 }}
             </Await>
         </div>
