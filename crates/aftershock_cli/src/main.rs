cd93ddba02bfa82a4f9e816ecbb62ba7d04583b3
aspirin <f1193968991@126.com>
2026-01-05 14:12:22 +0800

    refactor: better cli interface

--- crates/aftershock_cli/src/main.rs
+++ crates/aftershock_cli/src/main.rs
@@ -1,18 +1,34 @@
 use aftershock_cli::command::Cli;
 use aftershock_cli::command::Commands;
+use aftershock_cli::command::KindCommands;
 use aftershock_cli::requests::*;
 use clap::Parser;
 
 fn main() {
     let cli = Cli::parse();
 
-    let kind = cli.kind;
     match cli.command {
-        Commands::Add { path } => println!("{}", add(kind, path)),
-        Commands::List => println!("{}", list(kind)),
-        Commands::View { id } => println!("{}", view(kind, id)),
-        Commands::Delete { id } => println!("{}", delete(kind, id)),
-        Commands::Update { path, id } => println!("{}", update(kind, path, id)),
-        Commands::Publish { id } => println!("{}", publish(kind, id)),
+        KindCommands::Article { command } => {
+            let kind = "post".to_string();
+            match command {
+                Commands::Add { path } => println!("{}", add(kind, path)),
+                Commands::List => println!("{}", list(kind)),
+                Commands::View { id } => println!("{}", view(kind, id)),
+                Commands::Delete { id } => println!("{}", delete(kind, id)),
+                Commands::Update { path, id } => println!("{}", update(kind, path, id)),
+                Commands::Publish { id } => println!("{}", publish(kind, id)),
+            }
+        }
+        KindCommands::Page { command } => {
+            let kind = "page".to_string();
+            match command {
+                Commands::Add { path } => println!("{}", add(kind, path)),
+                Commands::List => println!("{}", list(kind)),
+                Commands::View { id } => println!("{}", view(kind, id)),
+                Commands::Delete { id } => println!("{}", delete(kind, id)),
+                Commands::Update { path, id } => println!("{}", update(kind, path, id)),
+                Commands::Publish { id } => println!("{}", publish(kind, id)),
+            }
+        }
     }
 }
