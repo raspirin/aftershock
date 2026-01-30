58cb43a5a84f1c229057b86ea255db9fb7351278
aspirin <f1193968991@126.com>
2026-01-30 14:19:55 +0800

    refactor: simplify archive_page

--- crates/aftershock/src/app/pages/archive_page.rs
+++ crates/aftershock/src/app/pages/archive_page.rs
@@ -1,5 +1,4 @@
 use leptos::prelude::*;
-use leptos::task::spawn_local;
 use leptos_router::{hooks::use_params, params::Params};
 
 use crate::{
@@ -15,77 +14,44 @@ struct TagParams {
     tag: Option<String>,
 }
 
-#[derive(Clone)]
-struct TagData {
-    tag: String,
-    posts: Vec<aftershock_bridge::PostMeta>,
-}
-
-#[derive(Clone, Copy, PartialEq)]
-enum LoadingState {
-    Initial,
-    Loading,
-    Success,
-    Error,
-}
-
 #[component]
 pub fn ArchivePage() -> impl IntoView {
     let params = use_params::<TagParams>();
     let (msg, _set_msg) = signal(String::from(MSG_ARCHIVE_PLACEHOLDER));
-    let (data, set_data) = signal::<Option<TagData>>(None);
-    let (loading_state, set_loading_state) = signal(LoadingState::Initial);
 
-    Effect::new(move |_| {
-        let tag_opt = params.read().as_ref().ok().and_then(|p| p.tag.clone());
-
-        match tag_opt {
-            None => {
-                set_loading_state.set(LoadingState::Error);
-                set_data.set(None);
+    let data = Resource::new(
+        move || params.read().as_ref().ok().and_then(|p| p.tag.clone()),
+        |tag| async move {
+            match tag {
+                None => Err(()),
+                Some(ref empty) if empty.is_empty() => Err(()),
+                Some(tag) => get_posts_meta_by_tag(tag).await.map_err(|_| ()),
             }
-            Some(ref empty) if empty.is_empty() => {
-                set_loading_state.set(LoadingState::Error);
-                set_data.set(None);
-            }
-            Some(tag) => {
-                let tag_clone = tag.clone();
-                set_loading_state.set(LoadingState::Loading);
-
-                spawn_local(async move {
-                    match get_posts_meta_by_tag(tag_clone.clone()).await {
-                        Ok(posts) => {
-                            set_data.set(Some(TagData {
-                                tag: tag_clone,
-                                posts,
-                            }));
-                            set_loading_state.set(LoadingState::Success);
-                        }
-                        Err(_) => {
-                            set_loading_state.set(LoadingState::Error);
-                            set_data.set(None);
-                        }
-                    }
-                });
-            }
-        }
-    });
+        },
+    );
 
     view! {
-        {move || {
-            match (loading_state.get(), data.get()) {
-                (LoadingState::Success, Some(ref data)) => {
-                    view! {
-                        <PostMetaListGroupByTag
-                            post_meta_list=data.posts.clone()
-                            primary_tag=data.tag.clone()
-                        />
-                    }
-                        .into_any()
-                }
-                (LoadingState::Error, None) => view! { <MessageBox msg=msg /> }.into_any(),
-                _ => view! { <div></div> }.into_any(),
-            }
-        }}
+        <Suspense>
+            {move || {
+                data.get()
+                    .map(|result| match result {
+                        Ok(posts) => {
+                            view! {
+                                <PostMetaListGroupByTag
+                                    post_meta_list=posts.clone()
+                                    primary_tag=params
+                                        .read()
+                                        .as_ref()
+                                        .ok()
+                                        .and_then(|p| p.tag.clone())
+                                        .unwrap_or_default()
+                                />
+                            }
+                                .into_any()
+                        }
+                        Err(_) => view! { <MessageBox msg=msg /> }.into_any(),
+                    })
+            }}
+        </Suspense>
     }
 }
