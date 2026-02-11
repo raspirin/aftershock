df46db1febe207d016721388394cfbf6398ee1b5
aspirin <f1193968991@126.com>
2026-02-11 11:39:23 +0800

    fix: set database connection max size to 1

--- crates/aftershock_storage/src/pool.rs
+++ crates/aftershock_storage/src/pool.rs
@@ -13,6 +13,7 @@ pub fn get_connection_pool() -> Pool<ConnectionManager<SqliteConnection>> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is expected.");
     let manager = ConnectionManager::<SqliteConnection>::new(database_url);
     Pool::builder()
+        .max_size(1)
         .build(manager)
         .expect("Could not build connection pool")
 }
